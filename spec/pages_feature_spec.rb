1a132059229c52b1c73a51dfbead8415360db833
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-11 22:57:57 +0000

    added test for show page

--- spec/pages_feature_spec.rb
+++ spec/pages_feature_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
-feature 'displaying Pages' do
+feature 'Pages' do
+	before(:all) { Page.create(title: "new page", content: "hello world") }
 
 	context 'index' do
-		before(:all) { Page.create(title: "new page", content: "hello world") }
 
-		it "should display all pages" do
+		it 'should display all pages' do
 			visit '/pages'
-			expect(page).to have_link "new page"
+			expect(page).to have_link 'new page'
+		end
+	end
+
+	context 'show' do
+		it 'should display a page' do
+			visit '/pages'
+			click_link 'new page'
+			expect(page).to have_content 'new page'
+			expect(page).to have_content 'hello world'
 		end
 	end
 end
