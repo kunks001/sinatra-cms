28ee8cc3c6aa7f2bf39e17a3f704bb0c34468d55
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-11 17:15:22 +0000

    added first test for new page

---
+++ spec/page_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe page do
+	let(:page){ Page.new(title: "new page", content: "hello world") }
+
+	subject { page }
+
+  it { should respond_to :title }
+  it { should respond_to :content}
+end
