7eeb96854827ed08cbc716472c2ada63b361a24d
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-11 23:46:47 +0000

    added test for creating new page

--- spec/pages_feature_spec.rb
+++ spec/pages_feature_spec.rb
@@ -25,8 +25,27 @@ feature 'Pages' do
     it 'should contain a form for a new page' do
       visit '/pages/new'
       expect(page).to have_title 'add page'
-      expect(page).to have_field("page[title]")
-      expect(page).to have_field("page[content]")
+      expect(page).to have_field('page[title]')
+      expect(page).to have_field('page[content]')
+    end
+
+    describe 'creating a page' do
+      before do
+        visit '/pages/new'
+        fill_in 'page[title]', with: 'new page'
+        fill_in 'page[content]', with: 'foobar'
+        click_button 'Create'
+      end
+
+      it 'should make the new page available' do
+        expect(page).to have_link 'new page'
+      end
+
+      it 'should have a title and content' do
+        click_button 'new page'
+        expect(page).to have_title 'new page'
+        expect(page).to have_content 'foobar'
+      end
     end
   end
 end
